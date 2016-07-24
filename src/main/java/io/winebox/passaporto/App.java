171cf3cef6c3cb89365045d9373f328335d505f1
aj1310 <aj_ortiz1310@icloud.com>
2016-07-24 10:24:01 -0500

    Add entry point for service

---
+++ src/main/java/io/winebox/passaporto/App.java
@@ -0,0 +1,10 @@
+package io.winebox.passaporto;
+
+/**
+ * Created by AJ on 7/24/16.
+ */
+public final class App {
+    public static final void main( String[] args ) {
+        System.out.println("Hello, world !!!");
+    }
+}
