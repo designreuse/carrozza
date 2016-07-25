4af2a87f96624add1d596a21b4b59cfff011bddf
aj1310 <aj_ortiz1310@icloud.com>
2016-07-24 19:12:36 -0500

    Group packages for function

--- src/main/java/io/winebox/passaporto/App.java
+++ src/main/java/io/winebox/passaporto/App.java
@@ -1,10 +1,19 @@
 package io.winebox.passaporto;
 
+import com.graphhopper.util.CmdArgs;
+import io.winebox.passaporto.services.routing.Ferrovia;
+
 /**
  * Created by AJ on 7/24/16.
  */
 public final class App {
     public static final void main( String[] args ) {
-        System.out.println("Hello, world !!!");
+        CmdArgs graphHopperArgs = new CmdArgs()
+                .put("osmreader.osm", "input/NewYork.osm")
+                .put("graph.location", "output/graph-cache")
+                .put("prepare.ch.weightings", "no")
+                .put("graph.flag_encoders", "car|turn_costs=true");
+
+        Ferrovia ferrovia = new Ferrovia(graphHopperArgs);
     }
 }
